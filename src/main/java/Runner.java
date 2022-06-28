9b3947c52ca3499c7b31a6c1e261f18e34f54872
Sercan Tuna <ssercantuna@hotmail.com>
2022-06-27 20:11:27 -0400

    with type casting

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,10 +1,14 @@
 public class Runner {
     public static void main(String[] args) {
         // your code here
-        var sercan = new User("sercan@gmail.com", "secret");
+
+        User sercan = new User("sercan@gmail.com", "secret");
         sercan.login("sercan@gmail.com", "wrongsecret");
         sercan.login("sercan@gmail.com", "secret");
-        var james = new Author("james@gmail.com", "secret2");
+        User instanceAuthor = new Author("james@gmail.com", "secret2");
+        // prints class Author
+        System.out.println(instanceAuthor.getClass().toString());
+        Author james = (Author) instanceAuthor;
         james.login("james@gmail.com", "secret2");
         james.createPost();
         james.createPost();
